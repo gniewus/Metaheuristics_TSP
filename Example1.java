2d44c8b4fe4d9e2639ed15b5f08907916280219a
Tomasz Tkaczyk <gniewus11@gmail.com>
2018-05-23 17:56:59 +0200

    clean up 2

--- Example1.java
+++ Example1.java
@@ -1,4 +1,5 @@
 import org.nlogo.app.App;
+
 public class Example1 {
     public static void main(String[] argv) {
         App.main(argv);
@@ -8,26 +9,41 @@ public class Example1 {
                         public void run() {
                             try {
                                 App.app().open(
-                                        "./Classic Traveling Salesman_2018_kommentiert.nlogo");
-                            }
-                            catch(java.io.IOException ex) {
+                                        "./Classic Traveling Salesman_2018_unkommentiert.nlogo");
+                            } catch (java.io.IOException ex) {
                                 ex.printStackTrace();
-                            }}});
+                            }
+                        }
+                    });
 
-           App.app().command("tsp2018Map");
+            App.app().command("tsp2018Map");
             App.app().command("set population-size 100");
+            App.app().command("set number-of-cycles 100");
+
             App.app().command("setup");
             App.app().command("go");
+            //App.app().command("calculate-distance");
             //App.app().command("calculate-fitness");
+            //App.app().command("update_stats");
 
-           // String msg = String.valueOf( App.app().report("min-fitness"));
-          //  System.out.println(msg);
-           // App.app().command("update_stats");
+            String av = "";
+            String max = "";
+            String min = "";
 
-        }
+            while (av == "") {
+                av = String.valueOf(App.app().report("get-avg"));
+                max = String.valueOf(App.app().report("get-best"));
+                min = String.valueOf(App.app().report("get-worst"));
+                System.out.println(max + " " + av + " " + min);
+                App.app().command("repeat 50 [go]");
+                // App.app().command("update_stats");
+            }
+            System.out.println("Ende");
 
-        catch(Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
+
 }
