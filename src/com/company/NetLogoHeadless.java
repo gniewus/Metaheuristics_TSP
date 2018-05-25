40e8d3f33c72e76f8b77aa6706c10e89f55fcd68
juria <juri1993@hotmail.de>
2018-05-25 14:15:08 +0200

    Added methods to create parameter arrays and execute all combinations

--- src/com/company/NetLogoHeadless.java
+++ src/com/company/NetLogoHeadless.java
@@ -3,93 +3,136 @@ package com.company;
 import org.nlogo.headless.HeadlessWorkspace;
 
 import java.awt.font.NumericShaper;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class NetLogoHeadless {
+
+    private static final HeadlessWorkspace WORKSPACE = HeadlessWorkspace.newInstance();
+
     public static void main(String[] argv) {
-        HeadlessWorkspace workspace =
-                HeadlessWorkspace.newInstance() ;
         try {
-            workspace.open(
-                    "./Classic Traveling Salesman_2018_kommentiert.nlogo");
-            workspace.command("tsp2018Map");
-            int[] array = new int[100];
+            WORKSPACE.open(//"./Classic Traveling Salesman_2018_kommentiert.nlogo");
+                    "src/models/Classic Traveling Salesman_2018_kommentiert.nlogo");
+            WORKSPACE.command("tsp2018Map");
+
+            executeAllParameterCombinations();
+
+            /*int[] array = new int[100];
             Arrays.setAll(array, i -> i + 1);
             array = Arrays.stream(array).map(i -> i * 100).toArray();
             System.out.println(array);
             System.out.println("popSize;numOfCycles;crossover;tournament;mutation;counter;best;av;min;bestFitness;terminated;bestResult;");
+
             long startTime = System.currentTimeMillis();
-            for(Integer pop : array)
-            {
-                execute(workspace,pop,100,1.0,2,1);
-
-
+            for(Integer pop : array) {
+                execute(pop,2,1,2,100);
             }
 
             long stopTime = System.currentTimeMillis();
             long elapsedTime = (stopTime - startTime)/1000;
 
-            System.out.println(elapsedTime+" Sec.");
+            System.out.println(elapsedTime+" Sec.");*/
 
-            workspace.dispose();
-        }
-        catch(Exception ex) {
-            System.out.println(ex);
-            ex.printStackTrace();
+            WORKSPACE.dispose();
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
         }
     }
 
-    private static void execute(HeadlessWorkspace workspace, int popSize, int numOfCycles, double crossover, int tournament , double mutation) throws InterruptedException {
+    private static void executeAllParameterCombinations() throws InterruptedException {
+        // build param arrays
+        int[] populationSizes = createParamArray(1, 1000, 2);
+        int[] tournamentSizes = createParamArray(2, 10, 2);
+        int[] crossoverRates = createParamArray(1, 100, 2);
+        int[] mutationsRates = createParamArray(0, 30, 2);
+        int[] numbersOfCycles = createParamArray(1, 1000, 2);
 
-        workspace.command("set population-size "+popSize);
-        workspace.command("set number-of-cycles "+numOfCycles);
-        workspace.command("set crossover-rate "+crossover);
-        workspace.command("set tournament-size "+tournament);
-        workspace.command("set mutation-rate "+mutation);
-        workspace.command("setup");
+        System.out.println("popSize;tournament;crossover;mutation;numOfCycles;counter;best;av;min;bestFitness;terminated;bestResult;");
+        long startTime = System.currentTimeMillis();
 
-        String params = popSize+" ; "+numOfCycles+" ; "+crossover+" ; "+tournament+" ; "+mutation;
-        String av = "";
-        String best = "";
-        String min = "";
-        String bestResult = "";
-        String bestFitness = "";
+        //execute all combinations
+        for (int populationSize : populationSizes) {
+            for (int tournamentSize : tournamentSizes) {
+                for (int crossoverRate : crossoverRates) {
+                    for (int mutationRate : mutationsRates) {
+                        for (int numberOfCycles : numbersOfCycles) {
+                            execute(populationSize, tournamentSize, crossoverRate, mutationRate, numberOfCycles);
+                        }
+                    }
+                }
+            }
+        }
 
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println(elapsedTime / 1000 + " Sec.");
+    }
 
-        int number_of_cycles = numOfCycles;
-        int repeat_step = number_of_cycles/10; // Each 10 ticks check if its still working
-        int counter=0;
+    private static void execute(int popSize, int tournament, int crossover, double mutation, int numOfCycles) throws InterruptedException {
+
+        WORKSPACE.command("set population-size " + popSize);
+        WORKSPACE.command("set number-of-cycles " + numOfCycles);
+        WORKSPACE.command("set crossover-rate " + crossover);
+        WORKSPACE.command("set tournament-size " + tournament);
+        WORKSPACE.command("set mutation-rate " + mutation);
+        WORKSPACE.command("setup");
+
+        String params = popSize + " ; " + tournament + " ; " + crossover + " ; " + mutation + " ; " + numOfCycles;
+        String av, best, worst, bestResult, bestFitness = "";
+
+        // I commented out everything with repeatStep, because I didn't understood it.
+        //int repeat_step = numOfCycles/10; // Each 10 ticks check if its still working
+        int counter = 0;
         Boolean terminated = false;
         String final_row = "";
         String last_row = "";
-        while (counter < repeat_step && !terminated) {
-            av = String.valueOf(workspace.report("get-avg"));
-            best = String.valueOf(workspace.report("get-best"));
-            min = String.valueOf(workspace.report("get-worst"));
-            bestResult = String.valueOf(workspace.report("get-best-result"));
-            bestFitness = String.valueOf(workspace.report("get-best-fitness"));
-            workspace.command("repeat "+repeat_step+" [go]");
+        //while (counter < repeat_step && !terminated) {
+        while (counter < numOfCycles && !terminated) {
+            av = String.valueOf(WORKSPACE.report("get-avg"));
+            best = String.valueOf(WORKSPACE.report("get-best"));
+            worst = String.valueOf(WORKSPACE.report("get-worst"));
+            bestResult = String.valueOf(WORKSPACE.report("get-best-result"));
+            bestFitness = String.valueOf(WORKSPACE.report("get-best-fitness"));
+            //WORKSPACE.command("repeat " + repeat_step + " [go]");
+            WORKSPACE.command("repeat " + numOfCycles + " [go]");
 
-            if(!String.valueOf(workspace.report("get-avg")).equals("0.0")){
-                last_row = counter+";"+best + ";" + av + ";" + min +";" + bestFitness + ";"+ terminated+";"+ bestResult;
-
-                counter++;
-            } else {
+            if (av.equals("0.0")) {
                 terminated = true;
-                final_row = counter+";"+best + ";" + av + ";" + min +";" + bestFitness + ";"+ terminated +";"+bestResult;
-                System.out.println(params+final_row);
+                final_row = counter + ";" + best + ";" + av + ";" + worst +";" + bestFitness + ";" + terminated + ";" + bestResult;
+                System.out.println(params + final_row);
+            } else {
+                last_row = counter + ";" + best + ";" + av + ";" + worst +";" + bestFitness + ";" + terminated + ";" + bestResult;
+                counter++;
             }
         }
-        if(!terminated){
-            System.out.println(params+last_row);
-        }
 
+        if (!terminated) {
+            System.out.println(params + last_row);
+        }
 
         //
 
     }
 
+    private static int[] createParamArray(int min, int max, int nrOfParams) {
+        if (nrOfParams <= 1) {
+            return new int[]{min};
+        }
+
+        int[] result = new int[nrOfParams];
+
+        double stepSize = (double) (max-min) / (nrOfParams-1);
+
+        double current = min;
+        for (int i = 0; i < nrOfParams; i++) {
+            result[i] = (int) Math.round(current);
+            current += stepSize;
+        }
+
+        return result;
+    }
 
 }
