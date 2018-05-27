f129e402088536d18cb90e2bdebc6569e27e1508
juria <juri1993@hotmail.de>
2018-05-28 00:24:52 +0200

    Resolve city names

--- src/com/company/NetLogoHeadless.java
+++ src/com/company/NetLogoHeadless.java
@@ -1,15 +1,17 @@
 package com.company;
 
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 import org.nlogo.headless.HeadlessWorkspace;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class NetLogoHeadless {
 
     private static final HeadlessWorkspace WORKSPACE = HeadlessWorkspace.newInstance();
+    private static final HashMap<String, String> cityMap = new HashMap<>();
 
     public static void main(String[] argv) {
+        setupCityMap();
         try {
             WORKSPACE.open("./Classic Traveling Salesman_2018_kommentiert.nlogo");
 
@@ -86,7 +88,7 @@ public class NetLogoHeadless {
             bestFitness = Double.parseDouble(String.valueOf(WORKSPACE.report("get-best-fitness")));
             double improvement = lastBestFitness / bestFitness;
             lastBestFitness = bestFitness;
-            bestResult = String.valueOf(WORKSPACE.report("get-best-result"));
+            bestResult = replaceNumbersWithCityNames(String.valueOf(WORKSPACE.report("get-best-result")));
 
             String row = duration + " ; " + best + " ; " + av + " ; " + worst + " ; " + bestFitness + " ; " + improvement + " ; " + bestResult;
 
@@ -111,6 +113,40 @@ public class NetLogoHeadless {
         }
 
         return result;
+    }
+
+    private static String replaceNumbersWithCityNames(String sequence) {
+        String result = "";
+        sequence = sequence.substring(1, sequence.length()-1).replaceAll(",","");
+        String[] numbers = sequence.split("\\s+");
+        for (String number : numbers) {
+            result += cityMap.get(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length()-2) + "]";
+    }
+
+    private static void setupCityMap() {
+        cityMap.put("0.0", "Berlin");
+        cityMap.put("1.0", "Prag");
+        cityMap.put("2.0", "Wien");
+        cityMap.put("3.0", "Budapest");
+        cityMap.put("4.0", "Warschau");
+        cityMap.put("5.0", "Riga");
+        cityMap.put("6.0", "Vilnius");
+        cityMap.put("7.0", "Mins");
+        cityMap.put("8.0", "Kiew");
+        cityMap.put("9.0", "Moskau");
+        cityMap.put("10.0", "Jekaterinenburg");
+        cityMap.put("11.0", "Rostow");
+        cityMap.put("12.0", "Saransk");
+        cityMap.put("13.0", "Sotschi");
+        cityMap.put("14.0", "StPetersburg");
+        cityMap.put("15.0", "Wolgograd");
+        cityMap.put("16.0", "Samara");
+        cityMap.put("17.0", "Nowgorod");
+        cityMap.put("18.0", "Kasan");
+        cityMap.put("19.0", "Kaliningrad");
+        cityMap.put("20.0", "Kischinau");
     }
 
 }
