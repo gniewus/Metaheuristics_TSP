fc3d985bca94e04242901f66b3bf090e94af1388
juria <juri1993@hotmail.de>
2018-05-27 02:37:29 +0200

    Completed execution of multiple param combis

--- src/com/company/NetLogoHeadless.java
+++ src/com/company/NetLogoHeadless.java
@@ -2,11 +2,7 @@ package com.company;
 
 import org.nlogo.headless.HeadlessWorkspace;
 
-import java.awt.font.NumericShaper;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class NetLogoHeadless {
 
@@ -16,104 +12,71 @@ public class NetLogoHeadless {
         try {
             WORKSPACE.open(//"./Classic Traveling Salesman_2018_kommentiert.nlogo");
                     "src/models/Classic Traveling Salesman_2018_kommentiert.nlogo");
-            WORKSPACE.command("tsp2018Map");
 
             executeAllParameterCombinations();
 
-            /*int[] array = new int[100];
-            Arrays.setAll(array, i -> i + 1);
-            array = Arrays.stream(array).map(i -> i * 100).toArray();
-            System.out.println(array);
-            System.out.println("popSize;numOfCycles;crossover;tournament;mutation;counter;best;av;min;bestFitness;terminated;bestResult;");
-
-            long startTime = System.currentTimeMillis();
-            for(Integer pop : array) {
-                execute(pop,2,1,2,100);
-            }
-
-            long stopTime = System.currentTimeMillis();
-            long elapsedTime = (stopTime - startTime)/1000;
-
-            System.out.println(elapsedTime+" Sec.");*/
-
             WORKSPACE.dispose();
         } catch (Exception e) {
-            System.out.println(e);
             e.printStackTrace();
         }
     }
 
-    private static void executeAllParameterCombinations() throws InterruptedException {
+    private static void executeAllParameterCombinations() throws InterruptedException, IOException {
         // build param arrays
-        int[] populationSizes = createParamArray(1, 1000, 2);
-        int[] tournamentSizes = createParamArray(2, 10, 2);
-        int[] crossoverRates = createParamArray(1, 100, 2);
-        int[] mutationsRates = createParamArray(0, 30, 2);
-        int[] numbersOfCycles = createParamArray(1, 1000, 2);
+        int[] populationSizes = createParamArray(3, 1000, 3);
+        int[] tournamentSizes = createParamArray(2, 10, 3);
+        int[] mutationsRates = createParamArray(0, 30, 3);
+        int[] crossoverRates = createParamArray(0, 100, 3);
+        int[] numbersOfCycles = createParamArray(8, 1000, 101);
 
-        System.out.println("popSize;tournament;crossover;mutation;numOfCycles;counter;best;av;min;bestFitness;terminated;bestResult;");
+        System.out.println("popSize ; tournament ; mutation ; crossover ; numOfCycles ; "
+                + "duration ; best ; av ; worst ; bestFitness ; bestResult ;");
+
         long startTime = System.currentTimeMillis();
 
         //execute all combinations
         for (int populationSize : populationSizes) {
             for (int tournamentSize : tournamentSizes) {
-                for (int crossoverRate : crossoverRates) {
+                    if (populationSize >= tournamentSize) {
                     for (int mutationRate : mutationsRates) {
-                        for (int numberOfCycles : numbersOfCycles) {
-                            execute(populationSize, tournamentSize, crossoverRate, mutationRate, numberOfCycles);
+                        for (int crossoverRate : crossoverRates) {
+                            //execute(populationSize, tournamentSize, crossoverRate, mutationRate, numbersOfCycles);
                         }
                     }
                 }
             }
         }
-
-        long elapsedTime = System.currentTimeMillis() - startTime;
-        System.out.println(elapsedTime / 1000 + " Sec.");
+        System.out.println("Gesamtdauer: " + (System.currentTimeMillis()-startTime) / 1000 + " Sec.");
     }
 
-    private static void execute(int popSize, int tournament, int crossover, double mutation, int numOfCycles) throws InterruptedException {
+    private static void execute(int popSize, int tournament, double mutation, int crossover, int[] numbersOfCycles) throws InterruptedException {
 
         WORKSPACE.command("set population-size " + popSize);
-        WORKSPACE.command("set number-of-cycles " + numOfCycles);
-        WORKSPACE.command("set crossover-rate " + crossover);
         WORKSPACE.command("set tournament-size " + tournament);
         WORKSPACE.command("set mutation-rate " + mutation);
+        WORKSPACE.command("set crossover-rate " + crossover);
+        WORKSPACE.command("set number-of-cycles " + numbersOfCycles[numbersOfCycles.length-1]);
+        WORKSPACE.command("tsp2018Map");
         WORKSPACE.command("setup");
 
-        String params = popSize + " ; " + tournament + " ; " + crossover + " ; " + mutation + " ; " + numOfCycles;
-        String av, best, worst, bestResult, bestFitness = "";
+        for (int i = 0; i < numbersOfCycles.length; i++) {
+            String params = popSize + " ; " + tournament + " ; " + mutation + " ; " + crossover + " ; " + numbersOfCycles[i] + " ; ";
+            String av, best, worst, bestResult, bestFitness = "";
 
-        // I commented out everything with repeatStep, because I didn't understood it.
-        //int repeat_step = numOfCycles/10; // Each 10 ticks check if its still working
-        int counter = 0;
-        Boolean terminated = false;
-        String final_row = "";
-        String last_row = "";
-        //while (counter < repeat_step && !terminated) {
-        while (counter < numOfCycles && !terminated) {
-            av = String.valueOf(WORKSPACE.report("get-avg"));
+            long startTime = System.currentTimeMillis();
+            WORKSPACE.command("repeat " + (numbersOfCycles[i] - (i > 0 ? numbersOfCycles[i-1] : 0)) + " [go]");
+
+            long duration = System.currentTimeMillis() - startTime;
             best = String.valueOf(WORKSPACE.report("get-best"));
+            av = String.valueOf(WORKSPACE.report("get-avg"));
             worst = String.valueOf(WORKSPACE.report("get-worst"));
-            bestResult = String.valueOf(WORKSPACE.report("get-best-result"));
             bestFitness = String.valueOf(WORKSPACE.report("get-best-fitness"));
-            //WORKSPACE.command("repeat " + repeat_step + " [go]");
-            WORKSPACE.command("repeat " + numOfCycles + " [go]");
+            bestResult = String.valueOf(WORKSPACE.report("get-best-result"));
 
-            if (av.equals("0.0")) {
-                terminated = true;
-                final_row = counter + ";" + best + ";" + av + ";" + worst +";" + bestFitness + ";" + terminated + ";" + bestResult;
-                System.out.println(params + final_row);
-            } else {
-                last_row = counter + ";" + best + ";" + av + ";" + worst +";" + bestFitness + ";" + terminated + ";" + bestResult;
-                counter++;
-            }
+            String row = duration + " ; " + best + " ; " + av + " ; " + worst + " ; " + bestFitness + " ; " + bestResult;
+
+            System.out.println(params + row);
         }
-
-        if (!terminated) {
-            System.out.println(params + last_row);
-        }
-
-        //
 
     }
 
